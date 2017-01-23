832637f4c45200b5d6ec1e8de602766aa690eb19
Ernesto Roldan Garcia <jeroldan@gmail.com>
2017-01-23 22:20:38 +0100

    field readly

--- packages/seito/src/js/components/field.js
+++ packages/seito/src/js/components/field.js
@@ -32,11 +32,11 @@ const FieldFactory = (WField) => {
       const icon = this.props.icon ? <Icon icon={this.props.icon} /> : '';
       const text = this.props.required ? `* ${this.props.label}` : this.props.label;
       const label = this.props.label ? <label htmlFor={this.props.id}>{text}</label> : '';
-      const actions = [<Icon icon="close" className="small"/>]
+      const actions = this.props.readOnly === true ? [] : [<Icon icon="close" className="small"/>];
       return (
         <div className="field">
           <div className="icon-box">{icon}</div>
-          <div className="field-box">
+          <div className={`field-box`}>
             <WField {...this.props} onChange={this.handleChange}/>
             {label}
           </div>
@@ -47,13 +47,13 @@ const FieldFactory = (WField) => {
   }
 }
 
-const Field = ({id, type='text', icon, label, value, required, onChange }) => {
+const Field = ({id, type='text', icon, label, value, required, onChange, readOnly }) => {
   const handleChange = (e) => {
     onChange(id, e.target.value);
   };
   const notEmpty = value && value.length > 0 ? 'notEmpty' : '';
   return (
-    <input id={id} type={type} value={value} onChange={handleChange} required={required} className={notEmpty}/>
+    <input id={id} type={type} value={value} onChange={handleChange} required={required} className={notEmpty} readOnly={readOnly}/>
   )
 }
 
