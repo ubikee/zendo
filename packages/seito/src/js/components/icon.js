cea80fca4e3559b82899603ea85f1007bafd40cf
Ernesto Roldan Garcia <jeroldan@gmail.com>
2017-02-06 12:35:03 +0100

    icon toggled property

--- packages/seito/src/js/components/icon.js
+++ packages/seito/src/js/components/icon.js
@@ -4,8 +4,9 @@ import './icon.scss';
 const Icon = (props) => {
   const iconset = props.set ? props.set : 'material-icons';
   const clickable = props.action ? 'clickable' : '';
+  const toggled = props.toggled ? 'toggled' : '';
   return (
-    <span className={`icon ${iconset} ${clickable} ${props.className}`} onClick={props.action}>{props.icon}</span>
+    <span className={`icon ${iconset} ${clickable} ${toggled} ${props.className}`} onClick={props.action}>{props.icon}</span>
   )
 }
 
